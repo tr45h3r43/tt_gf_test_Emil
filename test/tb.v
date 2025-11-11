1a7c58f262b39c4f03fb7d7973bd27dbe61e0dc4
tr45h3r43 <106329481+tr45h3r43@users.noreply.github.com>
2025-11-11 13:47:20 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_tr45h3r43_OsteKlokke user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
